83fe1fb4314a3e38aabfd21eed4b797dd6f88d6a
aldelaro5 <aldelaro5@gmail,com>
2020-12-31 02:02:03 -0500

    Added the corridor generator
    Always pick one corridor per room and always pick the longest one. If there's multiple, choose randomly between them.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LevelGenerator : MonoBehaviour
 {
@@ -8,18 +11,39 @@ public class LevelGenerator : MonoBehaviour
   {
     Floor,
     Wall,
-    Room
+    Room,
+    Corridor
   }
+
+  private enum CorridorDirection
+  {
+    Up = 0,
+    Right,
+    Down,
+    Left
+  }
+
+  private readonly List<CorridorDirection> directions = Enum.GetValues(typeof(CorridorDirection)).Cast<CorridorDirection>().ToList();
 
   private struct Room
   {
     public Vector2Int size;
-    public Vector2Int posTopLeft;
+    public Vector2Int posBottomLeft;
   }
+
+  private struct CorridorCandidate
+  {
+    public CorridorDirection direction;
+    public Vector2Int pos;
+    public List<Vector2Int> tiles;
+  }
+
+  private List<Room> rooms = new List<Room>();
 
   [SerializeField] private GameObject floorPrefab;
   [SerializeField] private GameObject wallPrefab;
   [SerializeField] private GameObject roomPrefab;
+  [SerializeField] private GameObject corridorPrefab;
 
   [SerializeField] [Min(1)] private int levelSize = 10;
   [SerializeField] [Min(1)] private int tileSize = 5;
@@ -36,13 +60,134 @@ public class LevelGenerator : MonoBehaviour
   {
     InitialiseGenerator();
     GenerateRooms();
+    GenerateCorridors();
     GenerateLevelFromTiles();
+  }
+
+  private void GenerateCorridors()
+  {
+    foreach (var room in rooms)
+    {
+      var possibleCorridors = FindAllPossibleCorridorsForRoom(room);
+      var nbrTilesLongestCorridor = possibleCorridors.Max(x => x.tiles.Count);
+      var longestCorridors = possibleCorridors.Where(x => x.tiles.Count == nbrTilesLongestCorridor).ToArray();
+      CorridorCandidate chosenCorridor = longestCorridors[Random.Range(0, longestCorridors.Length)];
+
+      foreach (var tilePos in chosenCorridor.tiles)
+        levelTiles[tilePos.x][tilePos.y] = Tile.Corridor;
+    }
+  }
+
+  private List<CorridorCandidate> FindAllPossibleCorridorsForRoom(Room room)
+  {
+    List<CorridorCandidate> possibleCorridors = new List<CorridorCandidate>();
+    foreach (var direction in directions)
+    {
+      switch (direction)
+      {
+        case CorridorDirection.Up:
+          if (room.posBottomLeft.y + room.size.y > levelSize)
+            break;
+
+          for (int x = room.posBottomLeft.x; x < room.posBottomLeft.x + room.size.x; x++)
+          {
+            int y = room.posBottomLeft.y + room.size.y;
+            CorridorCandidate candidate;
+            candidate.direction = CorridorDirection.Up;
+            candidate.pos = new Vector2Int(x, y);
+            candidate.tiles = new List<Vector2Int>();
+            while (y < levelSize)
+            {
+              if (levelTiles[x][y] != Tile.Room)
+                candidate.tiles.Add(new Vector2Int(x, y));
+              else
+                break;
+
+              y++;
+            }
+            possibleCorridors.Add(candidate);
+          }
+          break;
+        case CorridorDirection.Right:
+          if (room.posBottomLeft.x + room.size.x >= levelSize)
+            break;
+
+          for (int y = room.posBottomLeft.y; y < room.posBottomLeft.y + room.size.y; y++)
+          {
+            int x = room.posBottomLeft.x + room.size.x;
+            CorridorCandidate candidate;
+            candidate.direction = CorridorDirection.Right;
+            candidate.pos = new Vector2Int(x, y);
+            candidate.tiles = new List<Vector2Int>();
+            while (x < levelSize)
+            {
+              if (levelTiles[x][y] != Tile.Room)
+                candidate.tiles.Add(new Vector2Int(x, y));
+              else
+                break;
+
+              x++;
+            }
+            possibleCorridors.Add(candidate);
+          }
+          break;
+        case CorridorDirection.Down:
+          if (room.posBottomLeft.y - 1 < 0)
+            break;
+
+          for (int x = room.posBottomLeft.x; x < room.posBottomLeft.x + room.size.x; x++)
+          {
+            int y = room.posBottomLeft.y - 1;
+            CorridorCandidate candidate;
+            candidate.direction = CorridorDirection.Down;
+            candidate.pos = new Vector2Int(x, y);
+            candidate.tiles = new List<Vector2Int>();
+            while (y >= 0)
+            {
+              if (levelTiles[x][y] != Tile.Room)
+                candidate.tiles.Add(new Vector2Int(x, y));
+              else
+                break;
+
+              y--;
+            }
+            possibleCorridors.Add(candidate);
+          }
+          break;
+        case CorridorDirection.Left:
+          if (room.posBottomLeft.x - 1 < 0)
+            break;
+
+          for (int y = room.posBottomLeft.y; y < room.posBottomLeft.y + room.size.y; y++)
+          {
+            int x = room.posBottomLeft.x - 1;
+            CorridorCandidate candidate;
+            candidate.direction = CorridorDirection.Left;
+            candidate.pos = new Vector2Int(x, y);
+            candidate.tiles = new List<Vector2Int>();
+            while (x >= 0)
+            {
+              if (levelTiles[x][y] != Tile.Room)
+                candidate.tiles.Add(new Vector2Int(x, y));
+              else
+                break;
+
+              x--;
+            }
+            possibleCorridors.Add(candidate);
+          }
+          break;
+        default:
+          Debug.LogError("Unsuported corridor direction");
+          break;
+      }
+    }
+    return possibleCorridors;
   }
 
   private void GenerateRooms()
   {
     int targetNbrRoom = Random.Range(prefferedMinNbrRoom, maxNbrRoom + 1);
-    Room[] rooms = new Room[targetNbrRoom];
     int nbrRoomGenerated = 0;
     bool freeRoomKnownToExists = false;
     while (nbrRoomGenerated < targetNbrRoom)
@@ -57,50 +202,56 @@ public class LevelGenerator : MonoBehaviour
 
       int sizeX = Random.Range(MinRoomSize.x, MaxRoomSize.x + 1);
       int sizeY = Random.Range(MinRoomSize.y, MaxRoomSize.y + 1);
-      int posTopLeftX = Random.Range(0, levelSize - (sizeX - 1));
-      int posTopLeftY = Random.Range(0, levelSize - (sizeY - 1));
+      int posBottomLeftX = Random.Range(0, levelSize - (sizeX - 1));
+      int posBottomLeftY = Random.Range(0, levelSize - (sizeY - 1));
 
-      if (!IsRoomValid(sizeX, sizeY, posTopLeftX, posTopLeftY))
+      if (!IsRoomValid(sizeX, sizeY, posBottomLeftX, posBottomLeftY))
         continue;
 
       for (int x = 0; x < sizeX; x++)
       {
         for (int y = 0; y < sizeY; y++)
         {
-          if (x + posTopLeftX >= levelSize || y + posTopLeftY >= levelSize)
+          if (x + posBottomLeftX >= levelSize || y + posBottomLeftY >= levelSize)
             continue;
 
-          levelTiles[x + posTopLeftX][y + posTopLeftY] = Tile.Room;
+          levelTiles[x + posBottomLeftX][y + posBottomLeftY] = Tile.Room;
         }
       }
 
-      for (int i = 0; i < sizeY; i++)
+      PlaceWallsAroundRoom(sizeX, sizeY, posBottomLeftX, posBottomLeftY);
+      Room room = new Room()
       {
-        int rowPos = i + posTopLeftY;
-        if (posTopLeftX - 1 >= 0)
-          levelTiles[posTopLeftX - 1][rowPos] = Tile.Wall;
-
-        if (posTopLeftX + sizeX < levelSize)
-          levelTiles[posTopLeftX + sizeX][rowPos] = Tile.Wall;
-      }
-
-      for (int j = 0; j < sizeX; j++)
-      {
-        int columnPos = j + posTopLeftX;
-        if (posTopLeftY - 1 >= 0)
-          levelTiles[columnPos][posTopLeftY - 1] = Tile.Wall;
-
-        if (posTopLeftY + sizeY < levelSize)
-          levelTiles[columnPos][posTopLeftY + sizeY] = Tile.Wall;
-      }
-
-      rooms[nbrRoomGenerated].size.x = sizeX;
-      rooms[nbrRoomGenerated].size.y = sizeY;
-      rooms[nbrRoomGenerated].posTopLeft.x = posTopLeftX;
-      rooms[nbrRoomGenerated].posTopLeft.y = posTopLeftY;
+        size = new Vector2Int(sizeX, sizeY),
+        posBottomLeft = new Vector2Int(posBottomLeftX, posBottomLeftY)
+      };
+      rooms.Add(room);
 
       freeRoomKnownToExists = false;
       nbrRoomGenerated++;
+    }
+  }
+
+  private void PlaceWallsAroundRoom(int sizeX, int sizeY, int posBottomLeftX, int posBottomLeftY)
+  {
+    for (int i = 0; i < sizeY; i++)
+    {
+      int rowPos = i + posBottomLeftY;
+      if (posBottomLeftX - 1 >= 0)
+        levelTiles[posBottomLeftX - 1][rowPos] = Tile.Wall;
+
+      if (posBottomLeftX + sizeX < levelSize)
+        levelTiles[posBottomLeftX + sizeX][rowPos] = Tile.Wall;
+    }
+
+    for (int j = 0; j < sizeX; j++)
+    {
+      int columnPos = j + posBottomLeftX;
+      if (posBottomLeftY - 1 >= 0)
+        levelTiles[columnPos][posBottomLeftY - 1] = Tile.Wall;
+
+      if (posBottomLeftY + sizeY < levelSize)
+        levelTiles[columnPos][posBottomLeftY + sizeY] = Tile.Wall;
     }
   }
 
@@ -125,21 +276,21 @@ public class LevelGenerator : MonoBehaviour
     return false;
   }
 
-  private bool IsRoomValid(int sizeX, int sizeY, int posTopLeftX, int posTopLeftY)
+  private bool IsRoomValid(int sizeX, int sizeY, int posBottomLeftX, int posBottomLeftY)
   {
-    if (posTopLeftX >= levelSize - (sizeX - 1))
+    if (posBottomLeftX >= levelSize - (sizeX - 1))
       return false;
-    if (posTopLeftY >= levelSize - (sizeY - 1))
+    if (posBottomLeftY >= levelSize - (sizeY - 1))
       return false;
 
     for (int x = 0; x < sizeX; x++)
     {
       for (int y = 0; y < sizeY; y++)
       {
-        if (x + posTopLeftX >= levelSize || y + posTopLeftY >= levelSize)
+        if (x + posBottomLeftX >= levelSize || y + posBottomLeftY >= levelSize)
           continue;
 
-        Tile tile = levelTiles[x + posTopLeftX][y + posTopLeftY];
+        Tile tile = levelTiles[x + posBottomLeftX][y + posBottomLeftY];
         if (tile == Tile.Room || tile == Tile.Wall)
           return false;
       }
@@ -177,8 +328,7 @@ public class LevelGenerator : MonoBehaviour
 
       for (int j = 0; j < levelSize; j++)
       {
-        // The Z pos is inverted compared to the tile map
-        Vector3 pos = new Vector3(i * tileSize, 0, (levelSize - 1) * tileSize - (j * tileSize));
+        Vector3 pos = new Vector3(i * tileSize, 0, j * tileSize);
         Tile tile = levelTiles[i][j];
         GameObject prefab = null;
         switch (tile)
@@ -191,6 +341,9 @@ public class LevelGenerator : MonoBehaviour
             break;
           case Tile.Room:
             prefab = roomPrefab;
+            break;
+          case Tile.Corridor:
+            prefab = corridorPrefab;
             break;
           default:
             Debug.LogError("Unhandled tile type " + tile.ToString());
