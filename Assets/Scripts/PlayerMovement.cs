f8477d5ae56e0c920579fb5e12be55f701b7f7c0
aldelaro5 <aldelaro5@gmail,com>
2021-01-01 19:27:17 -0500

    Implement movement prevention into walls and enemies
    Thanks to raycast and box colliders

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,12 +15,28 @@ public class PlayerMovement : MonoBehaviour
   private void Update()
   {
     if (Input.GetKeyDown(KeyCode.UpArrow))
-      transform.position += transform.forward * lvlGenerator.TileSize;
+    {
+      RaycastHit hit;
+      if (Physics.Raycast(transform.position + Vector3.up, transform.forward, out hit, (float)lvlGenerator.TileSize))
+        print(hit.collider.gameObject.name);
+      else
+        transform.position += transform.forward * lvlGenerator.TileSize;
+    }
     else if (Input.GetKeyDown(KeyCode.DownArrow))
-      transform.position -= transform.forward * lvlGenerator.TileSize;
+    {
+      RaycastHit hit;
+      if (Physics.Raycast(transform.position + Vector3.up, -transform.forward, out hit, (float)lvlGenerator.TileSize))
+        print(hit.collider.gameObject.name);
+      else
+        transform.position -= transform.forward * lvlGenerator.TileSize;
+    }
     else if (Input.GetKeyDown(KeyCode.RightArrow))
+    {
       transform.Rotate(new Vector3(0, 90, 0));
+    }
     else if (Input.GetKeyDown(KeyCode.LeftArrow))
+    {
       transform.Rotate(new Vector3(0, -90, 0));
+    }
   }
 }
