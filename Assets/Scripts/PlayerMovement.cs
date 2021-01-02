df914afc543ca8e5bc24a9780645ec20286a04e9
aldelaro5 <aldelaro5@gmail,com>
2021-01-02 00:57:54 -0500

    Implement smooth movement and rotation
    Need to figure out how to make held input work

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,61 @@ public class PlayerMovement : MonoBehaviour
 {
   private LevelGenerator lvlGenerator;
 
+  [SerializeField]
+  [Min(0.01f)]
+  private float movementSpeed = 1f;
+
+  [SerializeField]
+  [Min(0.01f)]
+  private float turningSpeed = 1f;
+
+  private bool isMoving = false;
+  private bool isTurning = false;
+
   private void Start()
   {
     lvlGenerator = FindObjectOfType<LevelGenerator>();
   }
 
+  private IEnumerator MoveToDestination(Vector3 destination)
+  {
+    isMoving = true;
+    while (transform.position != destination)
+    {
+      transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * movementSpeed);
+      yield return null;
+    }
+    isMoving = false;
+    yield break;
+  }
+
+
+  private IEnumerator TurnToAngleAroundY(float angle)
+  {
+    isTurning = true;
+    Vector3 angles = transform.rotation.eulerAngles;
+    Quaternion destAngle = Quaternion.Euler(angles.x, angles.y + angle, angles.z);
+    while (transform.rotation.eulerAngles != destAngle.eulerAngles)
+    {
+      transform.rotation = Quaternion.RotateTowards(transform.rotation, destAngle, Time.deltaTime * turningSpeed);
+      yield return null;
+    }
+    isTurning = false;
+    yield break;
+  }
+
   private void Update()
   {
+    if (isMoving || isTurning)
+      return;
+
     if (Input.GetKeyDown(KeyCode.UpArrow))
     {
       RaycastHit hit;
       if (Physics.Raycast(transform.position + Vector3.up, transform.forward, out hit, (float)lvlGenerator.TileSize))
         print(hit.collider.gameObject.name);
       else
-        transform.position += transform.forward * lvlGenerator.TileSize;
+        StartCoroutine(MoveToDestination(transform.position + transform.forward * lvlGenerator.TileSize));
     }
     else if (Input.GetKeyDown(KeyCode.DownArrow))
     {
@@ -28,15 +69,15 @@ public class PlayerMovement : MonoBehaviour
       if (Physics.Raycast(transform.position + Vector3.up, -transform.forward, out hit, (float)lvlGenerator.TileSize))
         print(hit.collider.gameObject.name);
       else
-        transform.position -= transform.forward * lvlGenerator.TileSize;
+        StartCoroutine(MoveToDestination(transform.position - transform.forward * lvlGenerator.TileSize));
     }
     else if (Input.GetKeyDown(KeyCode.RightArrow))
     {
-      transform.Rotate(new Vector3(0, 90, 0));
+      StartCoroutine(TurnToAngleAroundY(90f));
     }
     else if (Input.GetKeyDown(KeyCode.LeftArrow))
     {
-      transform.Rotate(new Vector3(0, -90, 0));
+      StartCoroutine(TurnToAngleAroundY(-90f));
     }
   }
 }
